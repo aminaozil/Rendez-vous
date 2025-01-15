a42a7437467a416c5f8efd330d79c2ea22ade4a3
aminaozil <youmaminatambengue@gmail.com>
2025-01-15 21:08:58 +0000

    send email

--- authentification/urls.py
+++ authentification/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
 
     path("reset_password/", views.PasswordResetView.as_view(template_name='authentification/password_reset.html'), name="reset_password"),
-    path("reset_password_sent/", views.PasswordResetDoneView.as_view(template_name='authentification/password_reset_sent.html'), name="password_reset_done"),
+    path("reset_password_send/", views.PasswordResetDoneView.as_view(template_name='authentification/password_reset_sent.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>", views.PasswordResetConfirmView.as_view(template_name='authentification/password_reset_form.html'), name="password_reset_confirm"),
     path("reset_password_complete", views.PasswordResetCompleteView.as_view(template_name='authentification/password_reset_done.html'), name="password_reset_complete"),
 
